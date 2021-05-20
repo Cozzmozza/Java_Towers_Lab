653596230321ff26b27747a13ec26c0a2938a800
Katy Currie <katylcurrie@gmail.com>
2021-05-20 16:19:01 +0100

    1st extension started

---
+++ src/main/java/Booking.java
@@ -0,0 +1,13 @@
+public class Booking {
+
+    private int nightsBooked;
+    private Bedroom bedroom;
+
+    public Booking(int nightsBooked, Bedroom bedroom) {
+        this.nightsBooked = nightsBooked;
+        this.bedroom = bedroom;
+    }
+
+
+
+}
