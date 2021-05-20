9ddcf36bbf7fe8232f62296d16a74d4849d96ad3
Colin Morrison <colinjmmorris@gmail.com>
2021-05-20 11:37:44 +0100

    Room class completed

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,4 +1,35 @@
-public class Room {
+import java.util.ArrayList;
+
+public abstract class Room {
 //    Abstract class, has a capacity and a collection of Guests
 
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(int capacity, ArrayList<Guest> guests) {
+        this.capacity = capacity;
+        this.guests = guests;
+    }
+
+
+//    Add a new guest to a room
+    public void addGuestToRoom(Guest guest){
+        guests.add(guest);
+    }
+
+
+//    Remove a guest from a room
+    public void removeGuestFromRoom(Guest guest){
+        guests.remove(guest);
+    }
+
+
+//    GETTERS
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return guests;
+    }
 }
